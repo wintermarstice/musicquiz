b82136d79c9790508263558643ffaa55420621c3
Buğra Kadirhan <wintermarstice@gmail.com>
2022-11-29 22:13:50 +0300

    Tracing init

--- musicquiz/src/main.rs
+++ musicquiz/src/main.rs
@@ -5,10 +5,14 @@ mod musicquiz;
 use musicquiz::MusicQuiz;
 
 fn main() {
+    // Initialize tracing
+    tracing_subscriber::fmt::init();
+    
+
     let app_name = "Music Quiz";
     let mut native_options = NativeOptions::default();
     native_options.initial_window_size = Some(Vec2::new(540.0, 960.0));
 
     run_native(app_name, native_options, Box::new(|cc| Box::new(MusicQuiz::new(cc))));
-    
+
 }
